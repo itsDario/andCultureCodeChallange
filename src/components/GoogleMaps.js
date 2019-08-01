4a0855022e112072fc2e8b0546e5671ac961290b
github username <itsDario1@gmail.com>
2019-08-01 17:33:12 -0400

    added api key and crossed fingers

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const GoogleMaps = compose(
     withProps({
-        googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry&key=${process.env.REACT_APP_API_KEY}&ap,drawing,places`,
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry&key=AIzaSyDzWqOOCmaJNbuj26JQ75KkJPRHQJECKMA&ap,drawing,places`,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
